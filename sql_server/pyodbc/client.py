67bcb327b4d3c2074457f14fd1b2ba94bca2abc3
cramm0 <cramm0@d8ed95fc-f742-0410-923b-f1a0a12816d3>
2009-01-11 20:21:32 +0000

    Made some minimal client module modifications to be in line with Django r8989 and r8990.
    There is possibly more work to be done because we are using subprocess (available
    starting on Python 2.4) instead of os.execvp to execute the external command line
    DB client, but execvp doesn't work as we expect under win32 (something similar
    happens in the sqlite3 Django DB backend).

--- sql_server/pyodbc/client.py
+++ sql_server/pyodbc/client.py
@@ -4,16 +4,21 @@ import os
 import sys
 
 class DatabaseClient(BaseDatabaseClient):
+    if os.name=='nt':
+        executable_name = 'osql'
+    else:
+        executable_name = 'isql'
+
     def runshell(self):
+        user = settings.DATABASE_OPTIONS.get('user', settings.DATABASE_USER)
+        password = settings.DATABASE_OPTIONS.get('passwd', settings.DATABASE_PASSWORD)
         if os.name=='nt':
             db = settings.DATABASE_OPTIONS.get('db', settings.DATABASE_NAME)
-            user = settings.DATABASE_OPTIONS.get('user', settings.DATABASE_USER)
-            password = settings.DATABASE_OPTIONS.get('passwd', settings.DATABASE_PASSWORD)
             server = settings.DATABASE_OPTIONS.get('host', settings.DATABASE_HOST)
             port = settings.DATABASE_OPTIONS.get('port', settings.DATABASE_PORT)
             defaults_file = settings.DATABASE_OPTIONS.get('read_default_file')
 
-            args = ['osql']
+            args = [self.executable_name]
             if server:
                 args += ["-S", server]
             if user:
@@ -28,18 +33,10 @@ class DatabaseClient(BaseDatabaseClient):
                 args += ["-i", defaults_file]
         else:
             dsn = settings.DATABASE_OPTIONS.get('dsn', settings.DATABASE_ODBC_DSN)
-            user = settings.DATABASE_OPTIONS.get('user', settings.DATABASE_USER)
-            password = settings.DATABASE_OPTIONS.get('passwd', settings.DATABASE_PASSWORD)
-            args = ['isql -v %s %s %s' %(dsn, user, password)]
+            args = ['%s -v %s %s %s' % (self.executable_name, dsn, user, password)]
 
         import subprocess
         try:
-            retcode = subprocess.call(args, shell=True)
-            if retcode:
-                print >>sys.stderr, "error level:", retcode
-                sys.exit(retcode)
+            subprocess.call(args, shell=True)
         except KeyboardInterrupt:
             pass
-        except OSError, e:
-            print >>sys.stderr, "Execution failed:", e
-
