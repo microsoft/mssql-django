49dc857307fc705db885f27962798bad7b3be9b3
Michiya Takahashi <michiya.takahashi@gmail.com>
2015-02-10 15:51:25 +0900

    update the version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ CLASSIFIERS=[
 
 setup(
     name='django-pyodbc-azure',
-    version='1.0.13',
+    version='1.0.14',
     description='Django backend for Microsoft SQL Server and Azure SQL Database using pyodbc',
     long_description=open('README.rst').read(),
     author='Michiya Takahashi',
