741dc131640ebaaa73cb6246695de2f5751f5ed1
Michiya Takahashi <michiya.takahashi@gmail.com>
2015-11-20 11:38:16 +0900

    declare Python 3.5 support

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ CLASSIFIERS=[
     'Programming Language :: Python :: 3.2',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Topic :: Internet :: WWW/HTTP',
 ]
 
