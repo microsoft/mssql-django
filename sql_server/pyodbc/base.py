6101c18d0df5459526ba4494b5a0fa980d932091
Day Barr <github.com@daybarr.com>
2015-12-10 17:16:33 +0000

    Don't refuse to talk to SQL Server 2016
    To enable testing against the SQL Server 2016 Community Tech Previews, we need
    to allow connections to be made without raising a
    
    `NotImplementedError('SQL Server v13 is not supported.')`
    
    Tested with latest CTP 3.1 currently available from
    [here](https://www.microsoft.com/en-us/evalcenter/evaluate-sql-server-2016)

--- sql_server/pyodbc/base.py
+++ sql_server/pyodbc/base.py
@@ -147,6 +147,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         10: 2008,
         11: 2012,
         12: 2014,
+        13: 2016,
     }
 
     # https://azure.microsoft.com/en-us/documentation/articles/sql-database-develop-csharp-retry-windows/
