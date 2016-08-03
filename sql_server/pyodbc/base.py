07630b536570df883b4eb8733ec1edde774021b3
Michiya Takahashi <michiya.takahashi@gmail.com>
2016-08-04 02:14:36 +0900

    fix invalid escaping of special characters for lookup

--- sql_server/pyodbc/base.py
+++ sql_server/pyodbc/base.py
@@ -124,7 +124,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
     #
     # Note: we use str.format() here for readability as '%' is used as a wildcard for
     # the LIKE operator.
-    pattern_esc = r"REPLACE(REPLACE(REPLACE({}, '\', '\\'), '%%', '\%%'), '_', '\_')"
+    pattern_esc = r"REPLACE(REPLACE(REPLACE({}, '\', '[\]'), '%%', '[%%]'), '_', '[_]')"
     pattern_ops = {
         'contains': "LIKE '%%' + {} + '%%'",
         'icontains': "LIKE '%%' + UPPER({}) + '%%'",
