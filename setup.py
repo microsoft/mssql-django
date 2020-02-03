47c7c9fa8d79819b2d6bbd33c0f261090c92c638
Oskar Persson <oskar.persson@polken.se>
2020-02-03 15:33:25 +0100

    Bump version 2.7.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ CLASSIFIERS = [
 
 setup(
     name='django-mssql-backend',
-    version='2.6.0',
+    version='2.7.0',
     description='Django backend for Microsoft SQL Server',
     long_description=open('README.rst').read(),
     author='ES Solutions AB',
