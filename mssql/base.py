cd436a8d962811295dcabe9b5ca6ddd46704d6c6
Kapil Kaushik <linkwithkk@gmail.com>
2021-03-19 10:07:39 +0530

    Allow Trusted connection parameter oveririding
    Trusted connection parameter is automatically set to yes which causes authentication failure when a different authentication method is used. e.g. when using managed identities with azure sql and authentication as ActiveDirectoryMsi

--- mssql/base.py
+++ mssql/base.py
@@ -249,6 +249,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         user = conn_params.get('USER', None)
         password = conn_params.get('PASSWORD', None)
         port = conn_params.get('PORT', None)
+        trusted_connection = conn_params.get('Trusted_Connection', 'yes')
 
         options = conn_params.get('OPTIONS', {})
         driver = options.get('driver', 'ODBC Driver 13 for SQL Server')
@@ -289,7 +290,7 @@ class DatabaseWrapper(BaseDatabaseWrapper):
             cstr_parts['PWD'] = password
         else:
             if ms_drivers.match(driver):
-                cstr_parts['Trusted_Connection'] = 'yes'
+                cstr_parts['Trusted_Connection'] = trusted_connection
             else:
                 cstr_parts['Integrated Security'] = 'SSPI'
 
