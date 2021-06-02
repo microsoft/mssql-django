4d1d7007249d2c028a2ed416376c70d97993e648
Sicong Jia <jiasicong@gmail.com>
2021-06-02 15:16:10 -0700

    Mark current skipped tests expected failures

--- testapp/runners.py
+++ testapp/runners.py
@@ -1,7 +1,6 @@
 from django.test.runner import DiscoverRunner
 from django.conf import settings
 
-from unittest import skip
 import xmlrunner
 
 EXCLUDED_TESTS = getattr(settings, 'EXCLUDED_TESTS', [])
@@ -9,6 +8,14 @@ REGEX_TESTS = getattr(settings, 'REGEX_TESTS', [])
 
 ENABLE_REGEX_TESTS = getattr(settings, 'ENABLE_REGEX_TESTS', False)
 
+
+def MarkexpectedFailure():
+    def decorator(test_item):
+        def wrapper():
+            raise "Expected Failure"
+        wrapper.__unittest_expecting_failure__ = True
+        return wrapper
+    return decorator
 
 class ExcludedTestSuiteRunner(DiscoverRunner):
     def build_suite(self, *args, **kwargs):
@@ -18,10 +25,14 @@ class ExcludedTestSuiteRunner(DiscoverRunner):
             test_name = case._testMethodName
             if ENABLE_REGEX_TESTS:
                 if case.id() in EXCLUDED_TESTS:
-                    setattr(case, test_name, skip("Not supported")(getattr(case, test_name)))
+                    test_method = getattr(case, test_name)
+                    setattr(case, test_name, MarkexpectedFailure()(test_method))
+                    # setattr(case, test_name, expectedFailure2()(test_method))
             else:
                 if case.id() in EXCLUDED_TESTS + REGEX_TESTS:
-                    setattr(case, test_name, skip("Not supported")(getattr(case, test_name)))
+                    test_method = getattr(case, test_name)
+                    setattr(case, test_name, MarkexpectedFailure()(test_method))
+                    # setattr(case, test_name, expectedFailure2()(test_method))
             tests.append(case)
         suite._tests = tests
         return suite
