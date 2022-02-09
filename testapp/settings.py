cded353965daed2f951dff384b6baacab3fb9320
Jonathan Mah <jonathanmah99@gmail.com>
2022-02-09 14:44:14 -0800

    Fixed windows path error

--- testapp/settings.py
+++ testapp/settings.py
@@ -26,7 +26,7 @@ DATABASES = {
     },
     'sqlite': {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": str(BASE_DIR / "db.sqlite3-0"),
+        "NAME": os.path.join(BASE_DIR / "db.sqlitetest"),
     }
 }
 
