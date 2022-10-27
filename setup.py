312b9f2912ffb41894e1979492481aa8a77cdae5
mShan0 <96149598+mShan0@users.noreply.github.com>
2022-10-27 12:16:24 -0700

    bump required django version to include 4.1.x updates (#192)

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     license='BSD',
     packages=find_packages(),
     install_requires=[
-        'django>=2.2,<=4.1',
+        'django>=2.2,<4.2',
         'pyodbc>=3.0',
         'pytz',
     ],
