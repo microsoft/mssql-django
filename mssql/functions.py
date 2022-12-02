aeef2893374ef120542b7aaada6e5516b2eb5079
mShan0 <mark.shan19@gmail.com>
2022-12-02 14:56:32 -0800

    match native  function updates

--- mssql/functions.py
+++ mssql/functions.py
@@ -286,11 +286,17 @@ def bulk_update_with_default(self, objs, fields, batch_size=None, default=0):
         raise ValueError('bulk_update() cannot be used with primary key fields.')
     if not objs:
         return 0
+    for obj in objs:
+        obj._prepare_related_fields_for_save(
+            operation_name="bulk_update", fields=fields
+        )
     # PK is used twice in the resulting update query, once in the filter
     # and once in the WHEN. Each field will also have one CAST.
-    max_batch_size = connections[self.db].ops.bulk_batch_size(['pk', 'pk'] + fields, objs)
+    self._for_write = True
+    connection = connections[self.db]
+    max_batch_size = connection.ops.bulk_batch_size(['pk', 'pk'] + fields, objs)
     batch_size = min(batch_size, max_batch_size) if batch_size else max_batch_size
-    requires_casting = connections[self.db].features.requires_casted_case_in_updates
+    requires_casting = connection.features.requires_casted_case_in_updates
     batches = (objs[i:i + batch_size] for i in range(0, len(objs), batch_size))
     updates = []
     for batch_objs in batches:
@@ -300,9 +306,7 @@ def bulk_update_with_default(self, objs, fields, batch_size=None, default=0):
             when_statements = []
             for obj in batch_objs:
                 attr = getattr(obj, field.attname)
-                if not isinstance(attr, Expression):
-                    if attr is None:
-                        value_none_counter += 1
+                if not hasattr(attr, "resolve_expression"):
                     attr = Value(attr, output_field=field)
                 when_statements.append(When(pk=obj.pk, then=attr))
             if connections[self.db].vendor == 'microsoft' and value_none_counter == len(when_statements):
