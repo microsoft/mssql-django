0f4008ebfd9e51d2e688ca58255e2358572add61
Mark Shan <mark.shan19@gmail.com>
2023-10-04 12:29:03 -0700

    add _unique_supported function

--- mssql/schema.py
+++ mssql/schema.py
@@ -1322,3 +1322,27 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
             new_index_name = index_name.replace('[', '').replace(']', '').replace('.', '_')
             return new_index_name
         return index_name
+
+    def _unique_supported(
+        self,
+        condition=None,
+        deferrable=None,
+        include=None,
+        expressions=None,
+        nulls_distinct=None,
+    ):
+        return (
+            (not condition or self.connection.features.supports_partial_indexes)
+            and (
+                not deferrable
+                or self.connection.features.supports_deferrable_unique_constraints
+            )
+            and (not include or self.connection.features.supports_covering_indexes)
+            and (
+                not expressions or self.connection.features.supports_expression_indexes
+            )
+            and (
+                nulls_distinct is None
+                or self.connection.features.supports_nulls_distinct_unique_constraints
+            )
+        )
