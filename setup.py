3af2a1bafd1f04f4cd63b0fca42385ae9c5fed5d
mShan0 <96149598+mShan0@users.noreply.github.com>
2024-01-09 16:25:25 -0800

    bump version to 1.4

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mssql-django',
-    version='1.3',
+    version='1.4',
     description='Django backend for Microsoft SQL Server',
     long_description=long_description,
     long_description_content_type='text/markdown',
