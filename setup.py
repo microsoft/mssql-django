3c92823f06ae5540ac50f055f8764b15aa288ce2
Nandana Rao Lingampelli <t-nlingampel@microsoft.com>
2025-05-23 17:07:44 +0530

    Upgrade to django version 5.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     license='BSD',
     packages=find_packages(),
     install_requires=[
-        'django>=3.2,<5.1',
+        'django>=3.2,<5.2',
         'pyodbc>=3.0',
         'pytz',
     ],
