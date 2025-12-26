b95936010dedaf1f30a342afe92a72433b551789
Loïc Jaquemet <loic.jaquemet+github@gmail.com>
2025-12-26 06:30:47 -0700

    handle datetimeoffset timezone part
    This fixes the handling of the timezone part for a DATETIMEOFFSET type.

--- mssql/base.py
+++ mssql/base.py
@@ -88,7 +88,8 @@ def handle_datetimeoffset(dto_value):
     # Decode bytes returned from SQL Server
     # source: https://github.com/mkleehammer/pyodbc/wiki/Using-an-Output-Converter-function
     tup = struct.unpack("<6hI2h", dto_value)  # e.g., (2017, 3, 16, 10, 35, 18, 500000000)
-    return datetime.datetime(tup[0], tup[1], tup[2], tup[3], tup[4], tup[5], tup[6] // 1000)
+    return datetime.datetime(tup[0], tup[1], tup[2], tup[3], tup[4], tup[5], tup[6] // 1000,
+                             datetime.timezone(datetime.timedelta(hours=tup[7], minutes=tup[8])))
 
 
 class DatabaseWrapper(BaseDatabaseWrapper):
