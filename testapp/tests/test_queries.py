0930d4200da3ca98a5c871192ab8bd2261ac5d35
Gaurav Sharma <sharmag@microsoft.com>
2026-03-03 07:35:21 +0000

    TEST: Add db_default bulk_create regression coverage

--- testapp/tests/test_queries.py
+++ testapp/tests/test_queries.py
@@ -1,6 +1,9 @@
 import django.db.utils
-from django.db import connections, connection
-from django.test import TransactionTestCase, TestCase
+from django import VERSION
+from django.db import connections, connection, models
+from django.db.models.functions import Now
+from django.test import TransactionTestCase, TestCase, skipUnlessDBFeature
+from django.utils import timezone
 
 from ..models import Author, BinaryData
 
@@ -33,3 +36,77 @@ class TestBinaryfieldGroupby(TestCase):
     def test_varbinary(self):
         with connection.cursor() as cursor:
             cursor.execute(f"SELECT binary FROM {BinaryData._meta.db_table} WHERE binary = %s GROUP BY binary", [bytes("ABC", 'utf-8')])
+
+
+@skipUnlessDBFeature("supports_expression_defaults")
+class DbDefaultBulkCreateRegressionTests(TransactionTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        if VERSION < (6, 0):
+            cls.skipTest("db_default bulk insert alignment is Django 6.0+ specific")
+
+        class DbDefaultBulkInsertModel(models.Model):
+            name = models.CharField(max_length=100)
+            created_at = models.DateTimeField(db_default=Now())
+
+            class Meta:
+                app_label = "testapp"
+                db_table = "testapp_dbdefault_bulk_insert"
+                managed = False
+
+        cls.DbDefaultBulkInsertModel = DbDefaultBulkInsertModel
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                IF OBJECT_ID('testapp_dbdefault_bulk_insert', 'U') IS NOT NULL
+                    DROP TABLE testapp_dbdefault_bulk_insert
+                """
+            )
+            cursor.execute(
+                """
+                CREATE TABLE testapp_dbdefault_bulk_insert (
+                    id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+                    name NVARCHAR(100) NOT NULL,
+                    created_at DATETIME2 NOT NULL DEFAULT SYSDATETIME()
+                )
+                """
+            )
+
+    @classmethod
+    def tearDownClass(cls):
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                IF OBJECT_ID('testapp_dbdefault_bulk_insert', 'U') IS NOT NULL
+                    DROP TABLE testapp_dbdefault_bulk_insert
+                """
+            )
+        super().tearDownClass()
+
+    def test_db_default_field_excluded_and_included(self):
+        model = self.DbDefaultBulkInsertModel
+        created_at_quoted_name = connection.ops.quote_name("created_at")
+
+        with self.assertNumQueries(1) as ctx:
+            model.objects.bulk_create([model(name="foo"), model(name="bar")])
+
+        self.assertEqual(
+            ctx[0]["sql"].count(created_at_quoted_name),
+            1 if connection.features.can_return_rows_from_bulk_insert else 0,
+        )
+
+        with self.assertNumQueries(1) as ctx:
+            model.objects.bulk_create(
+                [
+                    model(name="baz", created_at=timezone.now()),
+                    model(name="qux"),
+                ]
+            )
+
+        self.assertEqual(
+            ctx[0]["sql"].count(created_at_quoted_name),
+            2 if connection.features.can_return_rows_from_bulk_insert else 1,
+        )
