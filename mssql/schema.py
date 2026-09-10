ed81517637f6d69f9ae8bacd9352e0d1a5ec919c
Rob Berwick <rob.berwick@hp.com>
2026-09-10 11:48:09 +0100

    Fix: support unbound schema alter fields

--- mssql/schema.py
+++ mssql/schema.py
@@ -407,7 +407,7 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
         """
         if old_field.name != new_field.name:
             self._rename_meta_index_references(
-                new_field.model, old_field.name, new_field.name
+                getattr(new_field, 'model', model), old_field.name, new_field.name
             )
         super().alter_field(model, old_field, new_field, strict=strict)
 
@@ -466,7 +466,7 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
         if (old_is_auto and not new_is_auto) or (not old_is_auto and new_is_auto):
             raise NotImplementedError("the backend doesn't support altering from %s to %s." %
                 (old_field.get_internal_type(), new_field.get_internal_type()))
-        meta_model = new_field.model
+        meta_model = getattr(new_field, 'model', model)
 
 
         meta_index_replacements = self._get_meta_index_replacements(model)
